3b69cc6700aae40b1f3e4b8daf6203d5211946cf
Onkar Chiroma <onkar.chiroma@MCL096.local>
2016-11-13 16:42:31 +0000

    quick and dirty hack for js INT and IN search functionality to search the US content but return urls to INT or IN - this is only a temporary solution whilst INT, IN and US have the same content and structure.

--- assets/js/dit-functions.js
+++ assets/js/dit-functions.js
@@ -385,6 +385,14 @@ function getResults(size, start) {
     searchInput = $('#searchInput').val(),
     gateway = "https://5dle4b7qu3.execute-api.eu-west-1.amazonaws.com/prod";
 
+    //temporary hack until IN and INT are added to AWS CloudSearch
+    var requestLanguage = language;
+    if (requestLanguage==='int'||requestLanguage==='in') {
+      language = 'us';
+      // console.log('request lang: ' + requestLanguage);
+      // console.log('search lang: ' + language);
+    }
+
   if (searchInput === '') {
     searchArea.html("");
   } else {
@@ -404,6 +412,21 @@ function getResults(size, start) {
 
           var searchResults = results.hits.hit;
           searchResults.forEach(function(result) {
+
+            //temporary hack until IN and INT are added to AWS CloudSearch
+            
+
+            if (requestLanguage==='int'||requestLanguage==='in') {
+              console.log('request lang: ' + requestLanguage);
+
+              var resultUrlPart = result.fields.url.slice(2),// being lazy here and not stripping out using substring and split as I know this is only for a specific use case ;)
+                  resultUrlFull = requestLanguage + resultUrlPart;
+                  // console.log(resultUrlPart);
+                  // console.log(resultUrlFull);
+                  result.fields.url = resultUrlFull;
+            }
+
+
             var htmlStr = '<div class="search-result"><h3><a href="/' + result.fields.url + '">' + result.fields.pagetitle + '</a></h3>' +
               '<p class="search-result-link">' + "www.great.gov.uk/" + result.fields.url + '</p>' +
               '<p class="search-result-snippet">' + (result.fields.intro ? results.fields.intro : '') + '</p></div>';
